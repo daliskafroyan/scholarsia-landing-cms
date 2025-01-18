3cdafe74696fa81eda2d3a9472d385cd1f7f78b1
Daliska F. Royan <firstaroyan09@gmail.com>
2025-01-18 23:32:58 +0700

    chore: add nix env

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Your application description";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            nodePackages.pnpm
+          ];
+
+          shellHook = ''
+            echo "Node.js development environment"
+            echo "Node.js version: $(node --version)"
+            echo "PNPM version: $(pnpm --version)"
+          '';
+        };
+      });
+}
